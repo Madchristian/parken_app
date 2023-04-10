732339b7cea195fa968839ea9b74107d68dc3b06
madchristian <christianstrube@icloud.com>
2023-04-11 00:45:55 +0200

    .

--- public/src/js/parkplatz.js
+++ public/src/js/parkplatz.js
@@ -5,6 +5,7 @@ let socket;
 let reconnectAttempts = 0;
 const MAX_RECONNECT_ATTEMPTS = 10;
 
+const markers = [];
 
 function initializeWebSocket() {
   if (socket && socket.readyState === WebSocket.OPEN) {
@@ -26,13 +27,13 @@ function initializeWebSocket() {
   });
 }
 
-async function deleteParkedCar(id, map) {
+async function deleteParkedCar(id, locationName, map) {
   try {
     const confirmDelete = confirm("Fahrzeug abgeholt?");
     if (!confirmDelete) {
       return;
     }
-    const response = await fetch(`/apiv3/delete-vehicle?id=${id}`, {
+    const response = await fetch(`/apiv3/delete-vehicle?id=${id}&locationName=${locationName}`, {
       method: "DELETE",
       headers: {
         'Content-Type': 'application/json'
@@ -42,19 +43,18 @@ async function deleteParkedCar(id, map) {
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-
     // Remove the marker from the map
-    const marker = markerClusterGroup.getLayer(id);
+    const marker = markers.find(marker => marker._leaflet_id === id);
     if (marker) {
-      marker.remove();
+      map.removeLayer(marker);
+      markerGroup.removeLayer(marker);
+      
     }
   } catch (error) {
     console.error("Error deleting parked car:", error);
     alert("Error deleting parked car");
   }
 }
-
-let markerClusterGroup;
 
 document.addEventListener("DOMContentLoaded", async function () {
   // Initialize the map
@@ -63,50 +63,54 @@ document.addEventListener("DOMContentLoaded", async function () {
     zoom: 15,
     maxZoom: 19
   });
-  markerClusterGroup = L.markerClusterGroup();
-  map.addLayer(markerClusterGroup);
-  // Add a tile layer to the map
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-    maxZoom: 19
-  }).addTo(map);
 
-  try {
-    startSpinner();
-
-    // Fetch all parked cars from the database
-    const response = await fetch("/apiv3/search-vehicle");
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const parkedCars = await response.json();
-
-    if (!Array.isArray(parkedCars.data)) {
-      throw new Error("Response is not an array of parked cars");
-    }
+  // Create a feature group to group the markers
+  const markerGroup = L.featureGroup().addTo(map);
 
 
-    // Calculate the bounding box for the coordinates
-    const coordinates = parkedCars.data
-      .map(car => [car.latitude, car.longitude])
-      .filter(coord => coord[0] && coord[1]); // Filter out invalid coordinates
+// Add a tile layer to the map
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+  maxZoom: 19
+}).addTo(map);
 
-    if (coordinates.length > 0) {
-      const markerBounds = L.latLngBounds(coordinates);
+try {
+  startSpinner();
 
-      // Set the map view to include all parked cars
-      map.fitBounds(markerBounds);
-    } else {
-      // Set the map view to a default location and zoom level
-      map.setView([51.1657, 10.4515], 6); // Deutschland
-    }
+  // Fetch all parked cars from the database
+  const response = await fetch("/apiv3/search-vehicle");
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  const parkedCars = await response.json();
 
-    // Add markers with custom icons for each parked car
-    parkedCars.data.forEach(car => {
+  if (!Array.isArray(parkedCars.data)) {
+    throw new Error("Response is not an array of parked cars");
+  }
+
+
+  // Calculate the bounding box for the coordinates
+  const coordinates = parkedCars.data
+    .map(car => [car.latitude, car.longitude])
+    .filter(coord => coord[0] && coord[1]); // Filter out invalid coordinates
+
+  if (coordinates.length > 0) {
+    const markerBounds = L.latLngBounds(coordinates);
+
+    // Set the map view to include all parked cars
+    map.fitBounds(markerBounds);
+  } else {
+    // Set the map view to a default location and zoom level
+    map.setView([51.1657, 10.4515], 6); // Deutschland
+  }
+
+  // Loop through the parked cars and create markers for each one
+  parkedCars.data.forEach(car => {
+    if (car.latitude && car.longitude) { // Prüfen Sie, ob die Koordinaten gültig sind
       const icon = createIcon(car.vehiclestatus, car.licensePlate, car.timestamp, car._id);
-      const marker = L.marker([car.latitude, car.longitude], { icon: icon });
-      markerClusterGroup.addLayer(marker);
+      const marker = L.marker([car.latitude, car.longitude], { icon: icon, licensePlate: car.licensePlate });
+      markerGroup.addLayer(marker);
 
       // Add an event listener to the delete button on the marker
       const deleteButton = document.getElementById(`delete-${car._id}`);
@@ -114,27 +118,29 @@ document.addEventListener("DOMContentLoaded", async function () {
         deleteButton.addEventListener('click', (event) => {
 
           event.stopPropagation(); // Prevent the click event from propagating to the marker itself
-          deleteParkedCar(car._id, map);
+          deleteParkedCar(car._id, car.locationName, map);
         });
       }
       marker._leaflet_id = car._id;
-    });
+    }
+  })
 
-    stopSpinner();
-    initializeWebSocket();
-    console.log(initializeWebSocket)
-  } catch (error) {
-    console.error(error);
-    // Wenn ein Fehler auftritt, stelle trotzdem eine Verbindung zum WebSocket her
-    initializeWebSocket();
-    stopSpinner();
-  }
+
+  stopSpinner();
+  initializeWebSocket();
+} catch (error) {
+  console.error(error);
+  // Wenn ein Fehler auftritt, stelle trotzdem eine Verbindung zum WebSocket her
+  initializeWebSocket();
+  stopSpinner();
+}
 });
+
 
 function connectWebSocket() {
   try {
     socket = new WebSocket(`wss://${window.location.host}/apiv3/vehicle-queue`);
-    console.log(WebSocket)
+    console.log(socket)
     socket.onerror = function (event) {
       console.error("WebSocket error observed:", event);
       alert("WebSocket connection error");
@@ -155,15 +161,17 @@ function connectWebSocket() {
             const car = carData;
 
             // Update the marker for the car or add a new marker if it doesn't exist yet
-            const existingMarker = markerClusterGroup.getLayer(car._id);
+            const existingMarker = markerGroup.getLayer(car._id);
 
             if (existingMarker) {
               existingMarker.setLatLng([car.latitude, car.longitude]);
             } else {
               const icon = createIcon(car.vehiclestatus, car.licensePlate, car.timestamp, car._id);
 
-              const marker = L.marker([car.latitude, car.longitude], { icon: icon });
-              markerClusterGroup.addLayer(marker);
+              const marker = L.marker([car.latitude, car.longitude], { icon: icon, licensePlate: car.licensePlate });
+              markerGroup.addLayer(marker);
+              markers.push(marker);
+
 
               // Add an event listener to the delete button on the marker
               const deleteButton = document.getElementById(`delete-${car._id}`);
@@ -176,14 +184,17 @@ function connectWebSocket() {
             }
           }
         } else if (receivedData.type === 'delete') {
-          const carId = receivedData.carId;
-
+          const carId = receivedData.id;
+          console.log("Removing marker with ID", carId);
           // Remove the marker for the deleted vehicle from the map
-          const marker = markerClusterGroup.getLayer(carId);
+          const marker = markerGroup.getLayer(carId);
           if (marker) {
-            markerClusterGroup.removeLayer(marker);
+            console.log("Removing marker", marker);
+            markerGroup.removeLayer(marker);
+            console.log("Removed marker", marker);
+          } else {
+            console.log("No marker found with ID", carId);
           }
-
         }
       } catch (error) {
         console.error("Error processing WebSocket message:", error);
@@ -207,6 +218,22 @@ function connectWebSocket() {
   } catch (error) {
     console.error("WebSocket connection error:", error);
     alert("WebSocket connection error");
+  }
+}
+
+document.getElementById('searchForm').addEventListener('submit', (event) => {
+  event.preventDefault();
+  searchForLicensePlate();
+});
+
+function searchForLicensePlate() {
+  const licensePlate = document.getElementById('searchInput').value.toUpperCase();
+  const marker = markerGroup.getLayers().find(marker => marker.options.licensePlate === licensePlate);
+
+  if (marker) {
+    map.setView(marker.getLatLng(), 18);
+  } else {
+    alert('Kennzeichen nicht gefunden');
   }
 }
 
