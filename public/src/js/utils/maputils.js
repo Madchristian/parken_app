ba5e21ab131520c895b35e7c4866e2e4c535a41f
madchristian <christianstrube@icloud.com>
2023-04-16 19:26:27 +0200

    ..

--- public/src/js/utils/maputils.js
+++ public/src/js/utils/maputils.js
@@ -1,13 +1,22 @@
-// mapUtils.js
+// maputils.js
+
+export let currentZIndex = 1000;
+
 export function searchForLicensePlate(map, licensePlate, markerGroup) {
-    const marker = markerGroup
-      .getLayers()
-      .find((marker) => marker.options.licensePlate === licensePlate);
-  
-    if (marker) {
-      map.setView(marker.getLatLng(), 19);
-    } else {
-      alert("Kennzeichen nicht gefunden");
-    }
+  const marker = markerGroup
+    .getLayers()
+    .find((marker) => marker.options.licensePlate === licensePlate);
+
+  if (marker) {
+    // Erstellen Sie eine Animation, die auf den Marker zoomt
+    const zoomDuration = 2.5; // Dauer der Animation in Sekunden
+    map.flyTo(marker.getLatLng(), 19, { duration: zoomDuration });
+
+    setTimeout(() => {
+      currentZIndex += 1;
+      marker.setZIndexOffset(currentZIndex);
+    }, zoomDuration * 1000); // Aktualisieren Sie den Z-Index nach der Animation
+  } else {
+    alert("Kennzeichen nicht gefunden");
   }
-  
+}
